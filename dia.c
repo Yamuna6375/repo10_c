2fa871dc07012ab90caad6577c3505352a2d1074
Yamuna63 <yamuna@s.amity.edu.com>
2025-07-29 22:08:13 +0530

    condition added

--- dia.c
+++ dia.c
@@ -4,4 +4,7 @@
 int main() {
 
 	int i, j, n = 3;
-
+      for(i = 1; i <= n; i++) {
+        for(j = i; j < n; j++) printf(" ");
+        for(j = 1; j <= 2*i-1; j++) printf("*");
+        printf("\n");
