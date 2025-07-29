01ce47b4bdcdce1693af728f5d020e24ae1faff9
Yamuna63 <yamuna@s.amity.edu.com>
2025-07-29 22:08:53 +0530

    printf statements added

--- dia.c
+++ dia.c
@@ -8,3 +8,8 @@ int main() {
         for(j = i; j < n; j++) printf(" ");
         for(j = 1; j <= 2*i-1; j++) printf("*");
         printf("\n");
+
+	  }
+    for(i = n-1; i >= 1; i--) {
+        for(j = n; j > i; j--) printf(" ");
+        for(j = 1; j <= 2*i-1; j++) printf("*");
